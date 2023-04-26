16cbecef01ec55ecb0499af01188e7da8d2bf944
Inderby <youk8253@gmail.com>
2023-04-26 19:35:58 +0900

    #16 - 게시글 뷰 관련 테스트 작성 오타
    마지막 테스트에 `@Disabled`이 들어가지 않았다.

--- src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
@@ -64,6 +64,7 @@ class ArticleControllerTest {
                 .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
